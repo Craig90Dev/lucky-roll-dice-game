4de5cd272db0267e0014920856caa515f964a6a7
Craig Farran <fazzer_70@hotmail.co.uk>
2024-03-14 16:26:42 +0000

    Feat: Create variables for rollDice function in script.js and rename images in assets folder

---
+++ script.js
@@ -0,0 +1,12 @@
+// Roll Dice
+function rollDice() {
+  //Takes the number from the user input for # of dice.
+  const numOfDice = document.getElementById("number-selector").value;
+  // Variables for the result and image divs.
+  const diceResult = document.getElementById("dice-result");
+  const diceImages = document.getElementById("dice-images");
+  // Empty arrays to store dice values and images.
+  const values = [];
+  const images = [];
+  
+}
