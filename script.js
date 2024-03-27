7c38f81662574d0830f670033e3c65678094712e
Craig Farran <fazzer_70@hotmail.co.uk>
2024-03-27 16:20:39 +0000

    Feat: Add JS code for generating random numbers function and checking player answers and updating current score functions

--- script.js
+++ script.js
@@ -3,23 +3,102 @@ const startButton = document.getElementById("start-btn");
 const welcomePage = document.getElementById("welcome-page");
 const gamePage = document.getElementById("game-page");
 const resetButton = document.getElementById("reset-btn");
+const currentElem = document.getElementById("dice-images");
+const higherLowerBtns = document.getElementsByClassName("higher-lower-btn");
+const resultElem = document.getElementById("result-area");
+let scoreElem = document.getElementById("score") 
+
+let score = 0;
+let currentNumber = generateRandomDice();
+currentElem.innerText = currentNumber;
 
 // Checks DOM content has loaded
 document.addEventListener("DOMContentLoaded", function() {
   initialiseEventListeners();
 });
 
+//Initialise event listeners once DOM has loaded
 function initialiseEventListeners() {
   startButton.addEventListener("click", startGame);
   resetButton.addEventListener("click", resetGame);
-}
 
+  // Enables user to use left and right arrow keys to make choice
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
+        let choice = event.key === "ArrowLeft" ? "lower" : "higher";
+        playGame(choice);
+    }
+});
+}
+// Hides welcome page and displays game interface on Start Game button click
 function startGame() {
   welcomePage.classList.add("hide");
   gamePage.classList.remove("hide");
 }
 
+// Hides game interface and displays welcome page on Reset Game button click
 function resetGame() {
   welcomePage.classList.remove("hide");
   gamePage.classList.add("hide");
+}
+
+for (let i = 0; i < higherLowerBtns.length; i++) {
+  higherLowerBtns[i].addEventListener("click", function() {
+    playGame(this.dataset.value);
+  });
+}
+
+function playGame(choice) {
+  // Generate new number for comparison
+  const currentNumber = parseInt(document.getElementById("dice-images").innerText); // parseInt to ensure is a number
+  let newNumber = generateRandomDice();
+  //Ensures new and old numbers are not the same
+  while (newNumber === currentNumber) {
+    newNumber = generateRandomDice();
+  }
+  //Update new number in images box
+  document.getElementById("dice-images").innerHTML = newNumber;
+
+  checkAnswer();
+  //Check if players answer is correct
+  function checkAnswer() {
+    if((currentNumber < newNumber && choice === "higher") || (currentNumber > newNumber && choice === "lower")) {
+      correctAnswer();
+    } else {
+      incorrectAnswer()
+    };
+  }
+}
+
+function correctAnswer() {
+  // Correct Answer and score update
+  score++;
+  scoreElem.innerText = score;
+  resultElem.innerText = "Correct!"
+  resultElem.classList.add("correct");
+  resultElem.classList.remove("hide");
+  timeoutFunction();
+}
+
+function incorrectAnswer() {
+  // Incorrect answer and score reset
+  score = 0;
+  scoreElem.innerText = score;
+
+  resultElem.innerText = "Incorrect!"
+  resultElem.classList.add("incorrect");
+  resultElem.classList.remove("hide");
+  timeoutFunction();
+}
+//Timeout function for results div to fade out
+function timeoutFunction() {
+  setTimeout(function () {
+    resultElem.classList.add("hide");
+    resultElem.classList.remove("correct");
+  }, 1000);
+}
+
+// function to generate random dice number
+function generateRandomDice() {
+  return Math.floor(Math.random() * 6) + 1;
 }
